4cdb0787d0c1e22bb3e0dee75d9e48d8c3dac874
hrujan <houska102@gmail.com>
2023-02-01 19:29:18 +0100

    FE - separated thje Project List into its own component

---
+++ kanban-frontend/src/components/Project/ProjectList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="projects">
+    <Project 
+      v-for="project in projects"
+      :key="project.id"
+      :id="project.id"
+      :name="project.name"
+      :description="project.description"
+      @projectClicked="handleProjectClick"
+    />
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router'
+import mockProjects from '@/MockData/Projects';
+import Project from '@/components/Project/Project.vue'
+
+export default defineComponent({
+  components: {
+    Project,
+  },
+  setup() {
+    const projects = ref(mockProjects);
+    const router = useRouter()    
+
+    const handleProjectClick = (id: number) => {
+      const projectRoute = `/project/${id}`;
+      router.push(projectRoute)
+    }
+    return {
+      projects,
+      handleProjectClick
+    }
+  },
+})
+</script>
+<style>
+  .projects {
+    display: flex;
+    flex-flow: row wrap;
+  }
+</style>
--- kanban-frontend/src/components/routes/Home.vue
+++ kanban-frontend/src/components/routes/Home.vue
@@ -1,37 +1,13 @@
 <template>
-  <section class="projects">
-    <Project 
-      v-for="project in projects"
-      :key="project.id"
-      :id="project.id"
-      :name="project.name"
-      :description="project.description"
-      @projectClicked="handleProjectClick"
-    />
-  </section>
+  <ProjectList />
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { useRouter } from 'vue-router'
-import mockProjects from '@/MockData/Projects';
-import Project from '@/components/Project/Project.vue'
+import { defineComponent } from 'vue';
+import ProjectList from '@/components/Project/ProjectList.vue'
 
 export default defineComponent({
   components: {
-    Project,
-  },
-  setup() {
-    const projects = ref(mockProjects);
-    const router = useRouter()    
-
-    const handleProjectClick = (id: number) => {
-      const projectRoute = `/project/${id}`;
-      router.push(projectRoute)
-    }
-    return {
-      projects,
-      handleProjectClick
-    }
+    ProjectList,
   },
 })
 </script>
